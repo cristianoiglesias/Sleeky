f7d90dcf5de8b29b23c1eaeaa0e68ce18ba5c7d9
cristianoiglesias <cristianoiglesias@users.noreply.github.com>
2016-11-13 10:58:37 -0200

    Update config.php

--- config.php
+++ config.php
@@ -5,16 +5,16 @@
 // GENERAL
 
 // Site URL
-define('siteURL', 'http://localhost:8888/');
+define('siteURL', 'http://mnto.co/de//');
 
 // Page title for your site
-define('title', 'Sleeky theme for YOURLS'); 
+define('title', 'Minito do Codigo > Shortener'); 
 
 // The short title of your site, used in the footer and in some sub pages
-define('shortTitle', 'Sleeky');
+define('shortTitle', 'Mnto.co/de');
 
 // A description of your site, shown on the homepage.
-define('description', 'A quick description on why your site is so fantastic, what it does and why people should definetly start using it. Oh, and how it’s free.'); 
+define('description', 'Compartilhe mais facilmente aquele codigo maroto que voc encontrou na net com seu amigo!'); 
 
 // The favicon for your site
 define('favicon', '/assets/img/favicon.ico');
@@ -29,9 +29,9 @@ define('logo', '/assets/img/logo-black.png');
 // The array follows a title link structure:
 // "TITLE" => "LINK",
 $footerLinks = [
-    "About" => "http://google.com",
-    "Contact" => "http://apple.com",
-    "Legal" => "http://example.com",
+    "About" => "http://ĉristianoiglesias.com",
+    "Contact" => "http://ĉristianoiglesias.com",
+    "Legal" => "http://ĉristianoiglesias.com",
 ];
 
 ?>
